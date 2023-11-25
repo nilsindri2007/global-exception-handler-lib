6c292db6acc6ad057ddd4f0df9462078363f3989
Nilesh Kumar <nilsindri.2007@gmail.com>
2023-11-26 04:33:51 +0530

    Initial commit

---
+++ src/main/java/com/example/globalexceptionhandlerlib/GlobalExceptionHandlerLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalexceptionhandlerlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalExceptionHandlerLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalExceptionHandlerLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/globalexceptionhandlerlib/GlobalExceptionHandlerLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globalexceptionhandlerlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalExceptionHandlerLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
